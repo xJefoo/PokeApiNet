e6158aaa7286079943f680dedfee8023eee558d0
Jeferson Nascimento <jeferson.pereiran@gmail.com>
2023-05-07 20:08:02 -0300

    adicionado chamada na program.cs para api

---
+++ PokemonApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using PokeApiNet;
+
+namespace PokemonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PokeApiClient();
+            var result = client.GetResourceAsync<Pokemon>("pikachu").Result;
+            Console.WriteLine($"Pikachu's ID is {result.Id}.");
+        }
+    }
+}
